3bd73fa62b5b578dc72c1d132d328b934ba305a2
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-06-11 10:30:27 +0530

    first commit

---
+++ calculator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main{
+    char ch;
+    cout<<"enter symbol";
+    cin>>ch;
+    int a,b;
+    cout<<"enter no."<<end;
+    cin>>a;
+
+
+
+    
+
+
+
+
+}
